cdf34ef92a0d167f2b3970d71f77b47568336596
huan <thaidaihuan@gmail.com>
2025-10-08 20:07:27 +0700

    [FE] SCD 118 model

--- src/app/(pages)/schedule/labels/components/lable.tsx
+++ src/app/(pages)/schedule/labels/components/lable.tsx
@@ -7,7 +7,14 @@ interface LabelProps {
 
 const Label = ({ label, icon, color }: LabelProps) => {
     return (
-        <div className={`flex justify-center gap-2 p-1 text-sm rounded-md border-2 text-[${color}] bg-[${color}]/20 border-[${color}]`}>
+        <div
+            className="flex justify-center gap-2 p-1 text-sm rounded-md border-2"
+            style={{
+                color: color,
+                backgroundColor: `${color}20`,
+                borderColor: color,
+            }}
+        >
             {icon}
             {label}
         </div>
