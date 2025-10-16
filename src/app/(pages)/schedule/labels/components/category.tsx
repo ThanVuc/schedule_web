fec4059dc9043e0e03b58e24e12543a98315912b
huan <thaidaihuan@gmail.com>
2025-10-16 15:06:01 +0700

    [FE] SCD-119 implement useLabelIcon

--- src/app/(pages)/schedule/labels/components/category.tsx
+++ src/app/(pages)/schedule/labels/components/category.tsx
@@ -1,22 +1,25 @@
+import { useLabelIcon } from "@/hooks";
 
 interface LabelProps {
-    label: string;
-    icon: string;
-    color: string;
+  label: string;
+  icon: string;
+  color: string;
 }
 
 export const Category = ({ label, icon, color }: LabelProps) => {
-    return (
-        <div
-            className="flex justify-center gap-2 p-1 text-sm rounded-md border-2"
-            style={{
-                color: color,
-                backgroundColor: `${color}20`,
-                borderColor: color,
-            }}
-        >
-            {icon}
-            {label}
-        </div>
-    );
-}
+  const IconComponent = useLabelIcon(icon);
+
+  return (
+    <div
+      className="flex items-center justify-center gap-2 p-1 text-sm rounded-md border-2"
+      style={{
+        color: color,
+        backgroundColor: `${color}20`,
+        borderColor: color,
+      }}
+    >
+      {IconComponent && <IconComponent />}
+      {label}
+    </div>
+  );
+};
