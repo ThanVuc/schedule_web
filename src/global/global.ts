b6966c18509961f19cb3eee15975d3de10e6b05f
sinh <sinh@gmail.com>
2025-11-09 16:24:34 +0700

    public .env

--- src/global/global.ts
+++ src/global/global.ts
@@ -4,13 +4,13 @@ export const globalConfig: GlobalConfig = {
     ApiUrlBase: process.env.NEXT_PUBLIC_API_URL_BASE ?? "http://localhost:3000/api/v1/",
     AppName: 'Schedulr',
     FirebaseConfig: {
-        apiKey: "AIzaSyA5eNXCUNWBrR41fTzapq0hZLVm5IX73h8",
-        authDomain: "notification-schedulr.firebaseapp.com",
-        projectId: "notification-schedulr",
-        storageBucket: "notification-schedulr.firebasestorage.app",
-        messagingSenderId: "244910001759",
-        appId: "1:244910001759:web:8b714c71679d8e988ad5ea",
-        measurementId: "G-N5QGEECZHB",
-        vapidKey: "BPupA3S0bMv4S2hqgrRber6SlSecjiQ-sVXdOx6MbCDUHyOKUifyPtEj2PMVfkGMFqk0c3lPZDSN95AxWyHxyhY"
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "",
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? "",
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? "",
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ?? "",
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? "",
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? "",
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? "",
+        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY ?? "",
     },
 };
