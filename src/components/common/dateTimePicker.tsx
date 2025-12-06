7ed5c2da0d58d6bf451180e8a41809ceeeeadbb9
sinh <sinh@gmail.com>
2025-12-06 12:25:56 +0700

    Revert "[FE][SCD-194]  fix output"
    This reverts commit 6e3aec9cd318fccaa0e5ec735091d040f252267c.

--- src/components/common/dateTimePicker.tsx
+++ src/components/common/dateTimePicker.tsx
@@ -27,7 +27,7 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
   defaultValue,
   onChange,
   title,
-  disabledTime = true,
+  disabledTime = false,
   disabledDate = false,
   icon,
 }) => {
@@ -68,7 +68,7 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
     }
 
     setDate(newDate);
-    { disabledDate && onChange?.(formatDate.dateToNumber(newDate) ?? undefined); }
+    onChange?.(formatDate.dateToNumber(newDate) ?? undefined);
   };
 
   return (
@@ -90,11 +90,9 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
 
             <div className="flex items-center w-full justify-between p-2">
               {date ? (
-                disabledTime
-                  ? format(date, "dd/MM/yyyy")      
-                  : format(date, "dd/MM/yyyy hh:mm aa") 
+                format(date, "MM/dd/yyyy hh:mm aa")
               ) : (
-                <span>{disabledTime ? "DD/MM/YYYY" : "DD/MM/YYYY hh:mm aa"}</span>
+                <span>MM/DD/YYYY hh:mm aa</span>
               )}
               {icon && <span className="ml-2">{icon}</span>}
             </div>
@@ -110,13 +108,6 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
             selected={date}
             disabled={disabledDate}
             initialFocus
-            onSelect={(d) => {
-              if (!d) return;
-              setDate(d);
-              if (!disabledDate) {
-                onChange?.(formatDate.dateToNumber(d) ?? undefined);
-              }
-            }}
           />
 
           {!disabledTime && <div className="flex flex-col sm:flex-row sm:h-[300px] divide-y sm:divide-y-0 sm:divide-x">
