c0c655406be8f987e9a54127e73221cd733a820d
sinh <sinh@gmail.com>
2026-03-07 11:54:58 +0700

    [TEAM] SCD-330: Init scrum team module

---
+++ src/app/(pages)/te/(features)/group/[id]/page.tsx
@@ -0,0 +1,13 @@
+export default async function GroupDetailRoute({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return (
+    <>
+      <h1>Hello, World with param: {id}</h1>
+    </>
+  );
+}
---
+++ src/app/(pages)/te/(features)/group/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+
+export default function GroupPage() {
+  return (
+    <>
+        <h1>Hello, World - Group!</h1>
+    </>
+  );
+}
