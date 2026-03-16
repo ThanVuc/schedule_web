42a7ea73d54c5cf2ca19a10f34de031fad565d95
sinh <sinh@gmail.com>
2026-03-16 23:09:36 +0700

    feat[Team Enum] SCD-401: Define the enum for team module

---
+++ src/app/(pages)/te/_constants/index.ts
@@ -0,0 +1,5 @@
+export * from "./groupRole";
+export * from "./sprintStatus";
+export * from "./userStatus";
+export * from "./workPriority";
+export * from "./workStatus";
---
+++ src/app/(pages)/te/_constants/workStatus.ts
@@ -0,0 +1,7 @@
+export enum WorkStatus {
+  UNSPECIFIED = 0,
+  TODO = 1,
+  IN_PROGRESS = 2,
+  IN_REVIEW = 3,
+  DONE = 4,
+}
